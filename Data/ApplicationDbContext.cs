78be504c4f9d712ef455f75eba890a7204e22f8f
SotoMonge <jssoto225@gmail.com>
2025-02-11 10:35:23 -0600

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCRUD.Models;
+namespace SimpleCRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+}
+}
